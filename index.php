da4d81c705710d9286d432aa9bef19ac7460fb7f
LauraNowak <73532414+LauraNowak@users.noreply.github.com>
2020-10-30 13:19:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,5 @@
 <?php
-
-echo("<li>Laura Nowak");
-echo("<li>Laura Nowak");
-echo("<li>Laura Nowak");
-echo("<li>Laura Nowak");
-
-$pudelko_01_w_pamieci_komputera = 12;
-
-$pudelko_02_w_pamieci_komputera = 3;
-
-$suma = $pudelko_01_w_pamieci_komputera + $pudelko_02_w_pamieci_komputera;
-echo ("<li>$suma");
-
-$roznica = $pudelko_01_w_pamieci_komputera - $pudelko_02_w_pamieci_komputera;
-echo ("<li>$roznica");
-
-$mnozenie = $pudelko_01_w_pamieci_komputera * $pudelko_02_w_pamieci_komputera;
-echo ("<li>$mnozenie");
-
-$dzielenie = $pudelko_01_w_pamieci_komputera / $pudelko_02_w_pamieci_komputera;
-echo ("<li>$dzielenie");
-
-$mojtext = "<h1>To jest zadanie z PHP:</h1>";
-$moj_inny_text = "<h2>Kalkulator</h2>";
-echo $mojtext.$moj_inny_text<?php
-    $conn = new mysqli("127.0.0.1","root","","Nauka1");  //Adres do którego się łączę, nazwa użytkownika, hasło, baza danych
+    $conn = new mysqli("https://www.alwaysdata.com/en/","root","","Nauka1");  //Adres do którego się łączę, nazwa użytkownika, hasło, baza danych
     $result = $conn->query('SELECT * FROM pracownicy');
         echo("<table border=1>");
         echo("<th>Id</th>"); //Nazwy Kolumn
@@ -39,7 +14,7 @@ echo $mojtext.$moj_inny_text<?php
             }
 
         echo("</table>");
-        $conn = new mysqli("127.0.0.1","root","","Nauka1");  
+        $conn = new mysqli("https://www.alwaysdata.com/en/","root","","Nauka1");  
     $result = $conn->query('SELECT * FROM pracownicy where imie like "%a"'); // wyświetla tylko kobiety
         echo("<table border=1>");
         echo("<th>Id</th>"); 
@@ -54,7 +29,7 @@ echo $mojtext.$moj_inny_text<?php
             }
 
         echo("</table>");
-        $conn = new mysqli("127.0.0.1","root","","Nauka1");  
+        $conn = new mysqli("https://www.alwaysdata.com/en/","root","","Nauka1");  
     $result = $conn->query('SELECT * FROM pracownicy where (dzial=1 or dzial=3)'); // wyświetla tylko pracowników z działu pierwszego i trzeciego
         echo("<table border=1>");
         echo("<th>Id</th>"); 
@@ -73,4 +48,3 @@ echo $mojtext.$moj_inny_text<?php
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
           }
-?>
